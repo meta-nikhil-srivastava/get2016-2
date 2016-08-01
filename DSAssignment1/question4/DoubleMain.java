f036313c6ed581514ec95b1c64218383ff055f1c
meta-ayushi <ayushi.agarwal@metacube.com>
2016-08-01 10:36:52 +0530

    Add files via upload

---
+++ DSAssignment1/question4/DoubleMain.java
@@ -0,0 +1,30 @@
+package Session1;
+
+public class DoubleMain {
+
+	public static void main(String args[])
+	{
+	DoublyLinkedList<String> Llist = new DoublyLinkedList<String>();
+	Llist.addAtFront("start");
+	Llist.add("a");
+	Llist.add("b");
+	Llist.add("c");
+	Llist.add("d");
+	Llist.add("e");
+	Llist.display();
+	System.out.println();
+	Llist.additemAtPosition(2, "ayushi");
+	Llist.display();
+	System.out.println();
+	Llist.removeByIndex(4);
+	
+	Llist.display();
+	System.out.println();
+	Llist.RemoveByValue("a");
+	Llist.display();
+	System.out.println();
+	Llist.reverseList();
+	
+	
+	}	
+}
