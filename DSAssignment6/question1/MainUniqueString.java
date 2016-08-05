f99736829754c4c12d4663afcf0ece9a14654475
meta-ayushi <ayushi.agarwal@metacube.com>
2016-08-05 17:53:45 +0530

    Add files via upload

---
+++ DSAssignment6/question1/MainUniqueString.java
@@ -0,0 +1,52 @@
+package session6;
+
+import java.util.Scanner;
+
+import session5.RecursiveSort;
+
+public class MainUniqueString
+{
+	
+	public static void main(String args[])
+	{
+		UniqueStringcharacters unique=new UniqueStringcharacters();
+		Scanner sc=new Scanner(System.in);
+		int option=0;
+		{
+			do{
+				do
+				{	
+					try
+					{
+						System.out.println("\nWhat do u want to perform:");
+						System.out.println("1. Add String");
+						System.out.println("2. Display No of unique character in string");
+						System.out.println("3. Exit");
+						option=sc.nextInt();
+					}
+					catch(Exception e)
+					{
+						System.out.println("Wrong choose entered");
+					}
+				}while(option<1&&option>3);
+			
+				if(option==1)
+				{
+					System.out.println("Enter the String to in list");
+					String value=sc.next();
+					unique.addString(value);
+				}
+				
+				else if(option==2)
+				{
+					unique.display();
+				}
+	
+				else if(option==3)
+				{
+					System.exit(0);
+				}
+			}while(option!=3);
+		}
+	}
+}
